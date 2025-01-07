7a5daa544cf9bf80fa70941572ae2318e09259c6
Andrei <andymoisa7@gmail.com>
2025-01-07 15:23:21 +0100

    typo error

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import { loginHandler } from "./logic/handlers/loginHandler.js";
 import { registerHandler } from "./logic/handlers/registerHandlers.js";
-import { fetchProfile } from "./logic/profile/profile.js";
+import { fetchProfile } from "./logic/posts/create.js";
 
 function router() {
   const pathname = window.location.pathname;
