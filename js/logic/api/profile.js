ff5887d994e3ed7cfa6452e0743dcd96726ec9e1
Andrei <andymoisa7@gmail.com>
2025-01-08 17:34:35 +0100

    profile handler fnc created

--- js/logic/api/profile.js
+++ js/logic/api/profile.js
@@ -1,26 +1,16 @@
 import { profileUrl } from "../../constants/constants.js";
 import { createFetchOptions } from "../utils/createFetchOptions.js";
-import { getToken, getUsername } from "../utils/storage.js";
+import { getUsername } from "../utils/storage.js";
 
 export async function fetchProfile() {
-  const token = getToken();
   const name = getUsername();
 
   const url = `${profileUrl}${name}`;
 
-  if (!token) {
-    console.log("No token, please log in");
-    throw new Error("No token provided");
-  }
-
-  const options = createFetchOptions(token);
-
-  console.log(options);
+  const options = createFetchOptions("GET");
 
   const response = await fetch(url, options);
   const json = await response.json();
-
-  console.log(json);
 
   if (!response.ok) {
     throw new Error(json.errors?.[0]?.message || "Registration failed");
---
+++ js/logic/handlers/profileHandler.js
@@ -0,0 +1,6 @@
+export async function profileHandler() {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+}
