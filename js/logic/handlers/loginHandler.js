ace98ed0dd811f4ae2650446307dd8bad02838a0
AndrewMoisa <113123678+AndrewMoisa@users.noreply.github.com>
2025-03-02 18:43:30 +0700

    typo error in loginHandler.js

--- js/logic/handlers/loginHandler.js
+++ js/logic/handlers/loginHandler.js
@@ -1,7 +1,8 @@
-/**
- * Initializes the login form handler by attaching an event listener to the form.
- * This function checks if the form with the ID `#loginForm` exists and adds a submit event listener.
- */
+import { loginUser } from "../api/loginUser.js";
+import { displayMessage } from "../../ui/shared/displayMessage.js";
+import { types } from "../../ui/shared/errorsStyles.js";
+import { saveToken, saveUsername } from "../utils/storage.js";
+
 export function loginHandler() {
   const form = document.querySelector("#loginForm");
 
@@ -10,14 +11,6 @@ export function loginHandler() {
   }
 }
 
-/**
- * Handles form submission for user login.
- *
- * Prevents the default form submission, gathers form data, and attempts to log in the user.
- * Displays messages based on success or failure.
- *
- * @param {Event} event - The form submit event.
- */
 async function submitForm(event) {
   event.preventDefault();
 
@@ -34,7 +27,6 @@ async function submitForm(event) {
   try {
     fieldset.disabled = true;
     button.textContent = "Loading...";
-
     const response = await loginUser(data);
     console.log(response);
     const { data: userData } = response;
@@ -45,10 +37,8 @@ async function submitForm(event) {
     saveToken(accessToken);
     saveUsername(name);
 
-    // Redirect to profile page upon successful login
     window.location.href = "../../../profile/";
   } catch (error) {
-    // Display error message if login fails
     displayMessage(
       containerMsg,
       types.error.classes,
@@ -56,7 +46,6 @@ async function submitForm(event) {
       types.error.icon
     );
   } finally {
-    // Re-enable form elements and reset button text
     fieldset.disabled = false;
     button.textContent = "Submit";
   }
